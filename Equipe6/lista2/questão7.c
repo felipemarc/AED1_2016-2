89f296a90cf917ab0bd70279462883acc8edbd7e
jailson <jailsoncolares@gmail.com>
2016-08-19 18:52:53 -0400

    questão 7 a terminar

---
+++ Equipe6/lista2/questão7.c
@@ -0,0 +1,31 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  questão7.c
+ *
+ *    Description:  
+ *
+ *        Version:  1.0
+ *        Created:  19-08-2016 18:14:52
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  YOUR NAME (), 
+ *   Organization:  
+ *
+ * =====================================================================================
+ */
+
+#include<stdio.h>
+#include<string.h>
+
+int qnt (char *x,char y){
+	
+
+}
+
+int main (int argc ,const char argv[]){
+	
+
+	return 0;
+}
