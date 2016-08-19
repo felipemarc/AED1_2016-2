d7d5def7bb62d9679dbc1ec2e35c54a369d51baf
jailson <jailsoncolares@gmail.com>
2016-08-19 14:40:20 -0400

    questão 8 da lista 2 a terminar

---
+++ Equipe6/lista2/questão8.c
@@ -0,0 +1,49 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  questão3.c
+ *
+ *    Description:  Fibonacci
+ *
+ *        Version:  1.0
+ *        Created:  19-08-2016 14:18:06
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author: 	Hélio Paulo
+ *         			Jailson Pereira
+ *         			Thiago Cauassa
+ *         			Rebeca Assunção
+ *   Organization:  
+ *
+ * =====================================================================================
+ */
+
+#include<stdio.h>
+
+int fibo(int x)
+{
+	if(x == 1)
+	{
+		return 0;
+	}
+	if (x == 2)
+	{
+		return 1;
+	}
+	if (x > 2)
+	{
+		return fibo(x-1)+fibo(x-2);
+	}
+}
+
+int main (int argc,const char argv[]){
+
+	int x, f;
+	scanf("%d",&x);
+	f = fibo(x);
+	printf("%d\n",f);
+	
+	
+	return 0;
+}
