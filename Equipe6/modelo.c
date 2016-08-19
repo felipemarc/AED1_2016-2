a8d462c9cd4b3613e4123cd0c3e4274cfcd6a391
jailson <jailsoncolares@gmail.com>
2016-08-19 14:10:17 -0400

    criação pasta equipe 6

---
+++ Equipe6/modelo.c
@@ -0,0 +1,22 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  modelo.c
+ *
+ *    Description:  modelo de cabeçalho
+ *
+ *        Version:  1.0
+ *        Created:  19-08-2016 14:07:12
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  Hélio Paulo
+ *         			Jailson Pereira
+ *         			Thiago Cauassa
+ *         			Rebeca Assunção
+ *   Organization:  
+ *
+ * =====================================================================================
+ */
+
+
