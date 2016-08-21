322c1a4042d876b5b0608787ef213b95eee706b4
diogo1992 <dias.eng@uea.edu.br>
2016-08-21 13:06:29 -0300

    resolução
    eu ainda vou padronizar

--- Equipe1/Lista2/ex1_lista2.c
+++ Equipe1/Lista2/ex1_lista2.c
@@ -1,4 +1,4 @@
-/* 	equipe 01:
+﻿/* 	equipe 01:
 	Diogo Abreu
 	Fernando Calderaro
 	Leticia Mota
@@ -8,6 +8,64 @@
 	exercicio 01
 	Impressão de um número natural em base binária
 */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int numero;
+
+int binario(int x)
+{
+    int y,k;
+    static int contador = 1;
+    static int w = 0;
+
+    if(x==0)
+    {
+        return 0;
+    }
+    if(x==1)
+    {
+        if(w==1)
+        {
+        contador = contador * 10;
+        return (1 * contador);
+        }
+        else
+        {
+            return 1;
+        }
+    }
+    if(x>1)
+    {
+        w = 1;
+        contador = contador  * 10;
+        y = (x%2) * contador + binario(x/2);
+    }
+    return y;
+}
+
+int main()
+{
+    int metade_numero ,resultado, z;
+    printf("- Conversão de numero decimal para binario -\n");
+    printf("Informe o valor do numero: \n");
+    scanf("%d",&numero);
+
+    if(numero==1)
+    {
+        metade_numero = numero;
+    }
+    else
+    {
+        metade_numero = numero/2;
+    }
+
+    resultado = binario(metade_numero);
+    z = (numero)%2;
+    printf("%d (decimal) = %d (binario)",numero,(resultado + z));
+    return 0;
+}
 
 
 
