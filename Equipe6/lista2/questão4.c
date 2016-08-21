2be0a3c29db2857814bf69d86101cf00758e671b
Cauassa <tiagocauassa@gmail.com>
2016-08-21 11:54:31 -0300

    questão 4 a terminar

---
+++ Equipe6/lista2/questão4.c
@@ -0,0 +1,28 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  questão4.c
+ *
+ *    Description:  modelo de cabeçalho
+ *
+ *        Version:  1.0
+ *        Created:  21-08-2016 11:07:12
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  Hélio Paulo
+ *         			Jailson Pereira
+ *         			Tiago Cauassa
+ *         			Rebeca Assunção
+ *   Organization:  
+ *
+ * =====================================================================================
+ */
+ // Dado um número n, gere todas as possíveis combinações com as n primeiras letras do alfabeto. Ex.: n = 3. Resposta: ABC, ACB, BAC, BCA, CAB, CBA
+#include <stdio.h>
+
+int main {
+
+
+
+}
