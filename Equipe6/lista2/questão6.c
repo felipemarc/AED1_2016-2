fef22693990110d5c98574344d898fd5b000403e
Cauassa <tiagocauassa@gmail.com>
2016-08-21 12:07:29 -0300

    questão 6 a terminar

---
+++ Equipe6/lista2/questão6.c
@@ -0,0 +1,31 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  questão6.c
+ *
+ *    Description:  modelo de cabeçalho
+ *
+ *        Version:  1.0
+ *        Created:  21-08-2016 12:01:12
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  Hélio Paulo
+ *         			Jailson Pereira
+ *					Rebeca Assunção         			
+ *			 		Tiago Cauassa
+ *         			
+ *   Organization:  
+ *
+ * =====================================================================================
+ */
+ // O máximo divisor comum dos inteiros x e y é o maior inteiro que é divisível por x e y. 
+ // Escreva uma função recursiva mdc em C, que retorna o máximo divisor comum de x e y. 
+ // O mdc de x e y é definido como segue: se y é igual a 0, então mdc(x,y) é x; caso contrário, mdc(x,y) é mdc (y, x%y), onde % é o operador resto.
+#include <stdio.h>
+
+int main {
+
+
+
+}
