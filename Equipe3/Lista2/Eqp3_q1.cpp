a95e88d1f17af35e68a28d12a03789a33d094c52
oliverlis88 <oliversouzalis@gmail.com>
2016-08-22 16:30:08 -0300

    lista 2 - q1  lógica incorreta
    vou alterar lógica

--- Equipe3/Lista2/Eqp3_q1.cpp
+++ Equipe3/Lista2/Eqp3_q1.cpp
@@ -1,17 +1,35 @@
 //*** Equipe 3 ***
-//Alerrandro Barreto
 //Caique Martins
 //Elisandra Oliveira
 //Frank Nápolis
 
 //Lista 2 - Recursividade
-//Questão 1 - Impressão de um número natural em base binária.
+//Questão 1 - Impressão de um número natural(decimal) em base binária.
 
-using namespace std;
-#include <cstdlib>
-#include <iostream>
-#include <iomanip>
 #include <stdio.h>
-#include <stdlib.h>
+#include <conio.h>
+
+int main (){
+
+	int n,bin,aux;
+
+	printf ("Este programa traduz o numero da base decimal para a base binaria.");
+	bin=aux=0;
+
+	printf ("Digite um numero na base decimal : ");
+	scanf ("%d",&n);
+
+	while (n > 0){
+
+		aux = n / 2;
+		bin = n%2;
+		printf ("%d",bin);
+
+
+		n = n / 2;
+		getch();
+	}
+	return 0;
+}
 
 
