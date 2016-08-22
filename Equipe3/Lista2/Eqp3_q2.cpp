fc737f401354a51e168f082618d8a6d99b457a27
oliverlis88 <oliversouzalis@gmail.com>
2016-08-22 16:36:15 -0300

    lista 2 - q2   laço errado
    vou corrigir o laço

--- Equipe3/Lista2/Eqp3_q2.cpp
+++ Equipe3/Lista2/Eqp3_q2.cpp
@@ -1,5 +1,4 @@
 //*** Equipe 3 ***
-//Alerrandro Barreto
 //Caique Martins
 //Elisandra Oliveira
 //Frank Nápolis
@@ -8,9 +7,26 @@
 //Questão 2 - Multiplicação de dois números naturais, através de somas
 // sucessivas (Ex.: 6 * 4 = 4+4+4+4+4+4)
 
-using namespace std;
-#include <cstdlib>
-#include <iostream>
-#include <iomanip>
 #include <stdio.h>
-#include <stdlib.h>
+#include <math.h>
+ 
+main()
+ 
+{
+  int num1 = 0, num2 = 0, soma = 0, i = 1;
+ 
+  printf("Digite o primeiro numero: ");
+  scanf(" %d", &num1);
+ 
+  printf("Digite o segundo numero: ");
+  scanf(" %d", &num2);
+ 
+  while (i<=num1)
+  {
+      printf(" %d X %d = %d", num1, num2, soma);
+ 
+      soma = soma + num2;
+      break;
+  }
+}
+
