3e43835faff34dae99a5a123ffbd2dbdaa24266a
oliverlis88 <oliversouzalis@gmail.com>
2016-08-22 00:53:41 -0300

    ainda não compilado
    falta editar código

---
+++ Equipe3/Lista2/Eqp3_q1.cpp
@@ -0,0 +1,17 @@
+//*** Equipe 3 ***
+//Alerrandro Barreto
+//Caique Martins
+//Elisandra Oliveira
+//Frank Nápolis
+
+//Lista 2 - Recursividade
+//Questão 1 - Impressão de um número natural em base binária.
+
+using namespace std;
+#include <cstdlib>
+#include <iostream>
+#include <iomanip>
+#include <stdio.h>
+#include <stdlib.h>
+
+
---
+++ Equipe3/Lista2/Eqp3_q2.cpp
@@ -0,0 +1,16 @@
+//*** Equipe 3 ***
+//Alerrandro Barreto
+//Caique Martins
+//Elisandra Oliveira
+//Frank Nápolis
+
+//Lista 2 - Recursividade
+//Questão 2 - Multiplicação de dois números naturais, através de somas
+// sucessivas (Ex.: 6 * 4 = 4+4+4+4+4+4)
+
+using namespace std;
+#include <cstdlib>
+#include <iostream>
+#include <iomanip>
+#include <stdio.h>
+#include <stdlib.h>
