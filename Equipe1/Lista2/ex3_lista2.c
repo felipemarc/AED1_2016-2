2bbaa81d9b1cade1fc0725347e20a669a8cb3ca4
FernandoCalderaro <fernandosilvacalderaro@gmail.com>
2016-08-23 16:34:05 -0400

    Falta a explicação

--- Equipe1/Lista2/ex3_lista2.c
+++ Equipe1/Lista2/ex3_lista2.c
@@ -1,4 +1,4 @@
-/* 	equipe 01:
+/*equipe 01:
 	Diogo Abreu
 	Fernando Calderaro
 	Leticia Mota
@@ -6,5 +6,35 @@
 	Thailana Neves		
 	
 	exercicio 03
-	Soma de dois nuÌmeros naturais, atraveÌs de incrementos sucessivos (Ex.: 3 + 2 = ++(++3)).
+	Soma de dois números naturais, através de incrementos sucessivos (Ex.: 3 + 2 = ++(++3)).
+ 
+ /Explicação
+ 
 */
+
+
+#include<stdio.h>
+#include<stdlib.h>
+
+int soma(int a, int b)
+{
+	if (a!=0)
+	{
+		soma(a-1,b+1);
+	}
+	else
+	{
+		return b;
+	}	
+}
+
+int main()
+{
+	int n1, n2;
+	
+	printf("Digite um numero: \n");
+	scanf("%d",&n1);
+	printf("Digite um numero: \n");
+	scanf("%d",&n2);
+	printf("%d \n", soma(n1,n2));
+}
