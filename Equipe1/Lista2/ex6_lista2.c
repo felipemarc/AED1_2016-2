4c18bac3f471e228dbd521ed8f1962b7978e3faf
leticiaamota <leticiaamota@gmail.com>
2016-08-22 22:17:59 -0400

    codigo exercicio 6 - funcionando

--- Equipe1/Lista2/ex6_lista2.c
+++ Equipe1/Lista2/ex6_lista2.c
@@ -1,4 +1,4 @@
-/* 	equipe 01:
+﻿/* equipe 01:
 	Diogo Abreu
 	Fernando Calderaro
 	Leticia Mota
@@ -6,7 +6,40 @@
 	Thailana Neves		
 	
 	exercicio 06
-	O máximo divisor comum dos inteiros x e y é o maior inteiro que é divisível por x e y. Escreva uma função recursiva mdc em C,
-	que retorna o máximo divisor comum de x e y. O mdc de x e y é definido como segue: se y é igual a 0, então mdc(x,y) é x;
-	caso contrário, mdc(x,y) é mdc (y, x%y), onde % é o operador resto.
+	
+	O máximo divisor comum dos inteiros x e y é o maior inteiro que é divisível por x e y. 
+	Escreva uma função recursiva mdc em C, que retorna o máximo divisor comum de x e y. 
+	O mdc de x e y é definido como segue: se y é igual a 0, então mdc(x,y) é x; caso contrário, mdc(x,y) é mdc (y, x%y), onde % é o operador resto.
+
 */
+
+#include <stdio.h>
+
+int mdc(int a, int b)
+{
+	int k;
+	if(b==0)
+	{
+		return a;
+	}else
+	{
+		k = mdc(b,(a%b));
+		return k;
+	}
+}
+
+int main()
+{
+	int x, y, z;
+	printf("FUNCAO MDC\n");
+	
+	printf("Insira o valor de X: ");
+	scanf("%d",&x);
+	printf("Insira o valor de Y: ");
+	scanf("%d",&y);
+	
+	z = mdc(x,y);
+	
+	printf("MDC de %d e %d: %d", x,y,z);
+	return 0;
+}
