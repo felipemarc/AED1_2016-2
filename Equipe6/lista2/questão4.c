c7d8e0b8ed56b13fd58133ebbf7c9e4e1970dc01
Cauassa <tiagocauassa@gmail.com>
2016-08-23 00:18:00 -0300

    que

--- Equipe6/lista2/questão4.c
+++ Equipe6/lista2/questão4.c
@@ -1,28 +1,49 @@
 /*
  * =====================================================================================
  *
- *       Filename:  questão4.c
+ *       Filename:  questÃ£o4.c
  *
- *    Description:  modelo de cabeçalho
+ *    Description:  modelo de cabeÃ§alho
  *
  *        Version:  1.0
  *        Created:  21-08-2016 11:07:12
  *       Revision:  none
  *       Compiler:  gcc
  *
- *         Author:  Hélio Paulo
+ *         Author:  HÃ©lio Paulo
  *         			Jailson Pereira
  *         			Tiago Cauassa
- *         			Rebeca Assunção
+ *         			Rebeca AssunÃ§Ã£o
  *   Organization:  
  *
  * =====================================================================================
  */
- // Dado um número n, gere todas as possíveis combinações com as n primeiras letras do alfabeto. Ex.: n = 3. Resposta: ABC, ACB, BAC, BCA, CAB, CBA
+ // Dado um nÃºmero n, gere todas as possÃ­veis combinaÃ§Ãµes com as n primeiras letras do alfabeto. Ex.: n = 3. Resposta: ABC, ACB, BAC, BCA, CAB, CBA
 #include <stdio.h>
 
 int main {
 
-
+	int a, b, c;
+	
+	if n=1
+	n=n
+	printf ("A")
+	else
+		if n=2
+		ab
+		if n=3
+		abc
+		if n=4
+		abcd
+		if n=5
+		abcde
+		if n=6
+		abcdef
+		if n=7
+		abcdefg
+		if n=8
+		abcdefgh
+		if n=9
+			
 
 }
