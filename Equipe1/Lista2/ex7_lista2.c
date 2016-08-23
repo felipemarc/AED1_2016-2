badb0d49e65b370504a4da407ad0ad9b30491a6f
rodrigooliveir <rdon.ads@uea.edu.br>
2016-08-22 21:11:18 -0400

    resolução

--- Equipe1/Lista2/ex7_lista2.c
+++ Equipe1/Lista2/ex7_lista2.c
@@ -3,9 +3,61 @@
 	Fernando Calderaro
 	Leticia Mota
 	Rodrigo Oliveira
-	Thailana Neves		
-	
+	Thailana Neves
+
 	exercicio 07
 	Escreva uma função recursiva que determine quantas vezes um dígito K ocorre em um número natural N.
 	Por exemplo, o dígito 2 ocorre 3 vezes em 762021192.
 */
+
+#include <stdio.h>
+
+//variaveis globais
+int k=0;
+int vezes=0;
+
+//funcao que verifica a quantidade de vezes que k ocorre em n
+int quantidade_k(int n)
+{
+    if(n==0)
+        return 0;
+    else
+    //compara o resto com k pois o resto sempre vai ser o
+    //ultimo numero de n
+    {
+    if(k==(n%10))
+    {
+        vezes++;
+    }
+    n=n/10;
+    return quantidade_k(n);
+    }
+    return 0;
+}
+
+int main()
+{
+    int n;
+
+    printf("-Programa que verifica a ocorrencia de um digito em um determinado numero-\n\n");
+    //o valor maximo que o tipo int recebe e 2147483647 que tem 10 digitos
+    printf("Digite um numero de ate 9 digitos:\n");
+    scanf("%d",&n);
+
+    printf("Digite o digito a ser verificado:\n");
+    scanf("%d",&k);
+
+    //chama a funcao
+    quantidade_k(n);
+
+    //mostra o resultado da verificacao
+    if (vezes==0)
+        printf("O digito %d nao ocorre em %d", k, n);
+    else
+        if (vezes==1)
+            printf("O digito %d ocorre %d vez em %d", k, vezes, n);
+        else
+            printf("O digito %d ocorre %d vezes em %d", k, vezes, n);
+
+    return 0;
+}
--- Equipe1/Lista2/ex8_lista2.c
+++ Equipe1/Lista2/ex8_lista2.c
@@ -3,8 +3,8 @@
 	Fernando Calderaro
 	Leticia Mota
 	Rodrigo Oliveira
-	Thailana Neves		
-	
+	Thailana Neves
+
 	exercicio 08
 	Uma sequência de Fibonacci generalizada, de f0 a f1 é definida como fibg(f0, f1, 0), fibg(f0, f1, 1), fibg(f0, f1, 2), ..., onde:
 	fibg(f0, f1, 0) = f0
@@ -12,3 +12,33 @@
 	fibg(f0, f1, n) = fibg(f0, f1, n-1) + fibg(f0, f1, n-2), se n > 1.
 	Escreva uma função recursiva em C para calcular fibg(f0, f1, n).
 */
+
+#include <stdio.h>
+
+int fibg(int n)
+{
+	if (n==0)
+		return 0;
+	if (n==1)
+		return 1;
+	if (n>1)
+	{
+		return fibg(n-1)+fibg(n-2);
+	}
+	return 0;
+}
+
+int main()
+{
+	int i, n;
+
+	printf("-Programa que calcula e mostra uma Sequência de Fibonacci generalizada-\n\n");
+	printf("Digite um número:\n");
+	scanf("%d",&n);
+
+	for(i=0;i<=n;i++)
+	{
+        printf("Sequência de Fibonacci generalizada: %d\n",fibg(i+1));
+	}
+	return 0;
+}
