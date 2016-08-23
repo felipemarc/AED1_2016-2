3126d8f6b6dc0b00fe3948aed0e2b33a8d647b6f
Stephanny Barreto <stephbarreto@me.com>
2016-08-23 16:43:31 -0400

    Update lista2q4.c

--- Equipe2/Lista2/lista2q4.c
+++ Equipe2/Lista2/lista2q4.c
@@ -1,3 +1,14 @@
+/*
+Equipe2
+Hermann Hernani
+Jessica Tavares
+Lucas Botinelly
+Paulo Marinho
+Stephanny Barreto
+
+4) Dado um número n, gere todas as possíveis combinações com as n primeiras letras do alfabeto. Ex.: n = 3. Resposta: ABC, ACB, BAC, BCA, CAB, CBA
+*/
+
 #include<stdio.h>
 
 int main(void)
