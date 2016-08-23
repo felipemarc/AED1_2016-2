c12b5e164beb33c704b453411d80267abbd8ef90
Botinelly <lucas_botinelly@hotmail.com>
2016-08-23 12:58:45 -0400

    lista2q5

---
+++ Equipe2/Lista2/lista2q5.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+
+void troca (int v[], int i, int j) {
+	int	aux;
+
+	aux = v[i];
+	v[i] = v[j];
+	v[j] = aux;
+}
+
+void mega (int v[], int n, int i) {
+	int	j;
+	if (i == n) 
+	{
+		for (j=0; j<n; j++) 
+			printf ("%d ", v[j]);
+		printf ("\n");
+	} 
+	else
+	{
+		for (j=i; j<n; j++) 
+		{
+			//Troca os valores do vetor
+			troca (v, i, j);
+			mega (v, n, i+1);
+			//Volta como era antes
+			troca (v, i, j);
+		}
+	}
+}
+			
+int main () {
+	int	vet[6], i;
+
+	for (i=0; i<6; i++)
+		vet[i] = i+1;
+	mega (vet, 6, 0);
+	return 0;
+}
