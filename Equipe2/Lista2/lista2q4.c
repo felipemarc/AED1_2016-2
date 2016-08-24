e807e2648622b898f7f5c1f51cbcdb62dee236de
jessiccatavares <jldst.snf@uea.edu.br>
2016-08-24 19:46:35 -0300

    Update lista2q4.c

--- Equipe2/Lista2/lista2q4.c
+++ Equipe2/Lista2/lista2q4.c
@@ -6,7 +6,9 @@ Lucas Botinelly
 Paulo Marinho
 Stephanny Barreto
 
-4) Dado um número n, gere todas as possíveis combinações com as n primeiras letras do alfabeto. Ex.: n = 3. Resposta: ABC, ACB, BAC, BCA, CAB, CBA
+4) Dado um número n, gere todas as possíveis combinações com as n primeiras letras do alfabeto.
+Ex.: n = 3. Resposta: ABC, ACB, BAC, BCA, CAB, CBA
+
 */
 
 #include<stdio.h>
