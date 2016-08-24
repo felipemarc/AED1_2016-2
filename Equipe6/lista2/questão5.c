107ebcdf345315d616624b3b8c8f1d5252c4df7b
Rebeca Assunção <rsva.lic@uea.edu.br>
2016-08-24 11:40:19 -0400

    Update questão5.c

--- Equipe6/lista2/questão5.c
+++ Equipe6/lista2/questão5.c
@@ -17,16 +17,48 @@
  *   Organization:  AED 1
  *
  * =====================================================================================
- */ 5) Gere todas as possíveis combinações para um jogo da MegaSena com 6 dezenas.
+ */ 
+ //5) Gere todas as possíveis combinações para um jogo da MegaSena com 6 dezenas.
 #include <stdio.h>
 #include <stdlib.h>
-void perm(int[]);
+
+void perm(int, int[]);
+void imprime (int[]);
+int troca (int, int[]);
 
 int main(int argc,const char *argv[]){
+
+  int dezenas[]={10,22,33,44,55,66};
+	
+	perm(dezenas);
 
   return 0; 
 }
 
-void perm(){
-  
+void perm(int dezenas[]){
+	for(int i=5; i>=0;i--){
+		if(i==0){
+			return 0;
+		}else{
+			imprime(dezenas[]);
+			troca(i,dezenas[]);
+			return perm();
+		}
+	}
 }
+	
+int troca (int i, int vetnovo[]){
+	int aux;
+	aux = vetnovo[i];
+	vetnovo[i] = vetnovo[i+1];
+	vetnovo[i+1] = aux;
+	
+	return vetnovo[];
+}
+
+void imprime(int vetor[]){
+	for(int i=0; i<=5;i==){
+		printf("%d  ",vetor[i]);
+	}
+}
+  
