4027a1cc8d573d1a64d299c7d84f173c85f93623
jessiccatavares <jldst.snf@uea.edu.br>
2016-08-24 19:56:04 -0300

    Update lista2q6.c

--- Equipe2/Lista2/lista2q6.c
+++ Equipe2/Lista2/lista2q6.c
@@ -1,5 +1,16 @@
-//6) O máximo divisor comum dos inteiros x e y é o maior inteiro que é divisível por x e y. Escreva uma função recursiva mdc em C, que retorna o máximo divisor comum de x e y. O mdc de x e y é definido como segue: se y é igual a 0, então mdc(x,y) é x; caso contrário, mdc(x,y) é mdc (y, x%y), onde % é o operador resto.
+/*
+Equipe 2
 
+Hermann Hernani
+Lucas Botinelly
+Jéssica Tavares
+Paulo Marinho
+Stephanny Barreto
+
+6) O máximo divisor comum dos inteiros x e y é o maior inteiro que é divisível por  x e y. 
+Escreva uma função recursiva mdc em C, que retorna o máximo divisor comum de x e y. O mdc de x e y é definido como segue: se y é igual a 0, então mdc(x,y) é x; caso contrário, mdc(x,y) é mdc (y, x%y), onde % é o operador resto.
+
+*/
 #include<stdio.h>
 
 int mdc(int num1, int num2)
