f09fa776b643a44da308b753dcf85b1add479dfe
bigustorm <paulo.roberto.uea@gmail.com>
2016-08-23 23:40:04 -0400

    Código pronto

--- equipe7/Lista_02/Exe02.c
+++ equipe7/Lista_02/Exe02.c
@@ -1,1 +1,33 @@
+/* equipe:
+    Fabiana Bezerra
+    Paulo Roberto
+    Paulo Silveira
+    Pedro Sena
+    
+    2. Multiplicação de dois números naturais, através de somas sucessivas (Ex.: 6 ∗ 4 = 4+4+4+4+4+4)
+*/
 
+#include <stdio.h>
+#include <stdlib.h>
+
+int multiplicacao (int num1, int num2)
+{
+  if(num2==0) return 0;
+  if(num2==1) return num1;
+  if(num2>1) return num1+multiplicacao(num1, num2-1);
+  //A medida que volta da pilha, soma os números
+}
+
+int main()
+{
+  int n, m, resultado;
+  
+  printf("Digite o multiplicando: \n");
+  scanf("%d", &n);
+  printf("Digite o multiplicado: \n");
+  scanf("%d", &m);
+  
+  resultado = multiplicacao(n, m);
+  
+  printf("O resultado é: ", resultado);
+}
