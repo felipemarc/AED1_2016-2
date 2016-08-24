c7b152f2da68acd7038c6667c61012032ce4e8dc
Cauassa <tiagocauassa@gmail.com>
2016-08-24 20:31:53 -0300

    Concluída

--- Equipe6/lista2/questão4.c
+++ Equipe6/lista2/questão4.c
@@ -20,29 +20,52 @@
  */
  // Dado um número n, gere todas as possíveis combinações com as n primeiras letras do alfabeto. Ex.: n = 3. Resposta: ABC, ACB, BAC, BCA, CAB, CBA
 #include <stdio.h>
+#include <stdlib.h>
 
-int main {
-	int a, b, c;
+void Troca(int v[],int i,int j) {
+	int t;
+	t = v[i];
+	v[i] = v[j];
+	v[j] = t;
+}
+
+void Gira_Esquerda(int v[],int go,int n) {
+	int tmp = v[go];
+	for (int i=go; i<n; i++){
+		v[i] = v[i+1];
+	}
+	v[n] = tmp;
+}
+void Imprima(int s[], int k) {
+	int i;
+	printf("\n");
+	for (i=1; i<=k; i++){
+		printf("%c", s[i]+'A'-1);
+	}
+}
+
+void Permuta(int v[],int inicio, int n) {
+	Imprima(v,n);
+	if (inicio<n) {
+		int i,j;
+		for(i=n-1; i>=inicio; i--) {
+			for(j=i+1; j<=n; j++) {
+				Troca(v,i,j);
+				Permuta(v,i+1,n);
+			}
+			Gira_Esquerda(v,i,n);
+		}
+	}
+}
+int main() {
+	int * s;
+	int N, i;
 	
-	if n=1
-	n=n
-	printf ("A")
-	else
-		if n=2
-		ab
-		if n=3
-		abc
-		if n=4
-		abcd
-		if n=5
-		abcde
-		if n=6
-		abcdef
-		if n=7
-		abcdefg
-		if n=8
-		abcdefgh
-		if n=9
-			
-
+	printf("\nEntre com n: ");
+	scanf("%d",&N);
+	
+	s = (int*)malloc((N+1)*sizeof(int));
+	
+	for (i=1; i<=N; i++) s[i] = i;
+	Permuta (s, 1, N);
 }
