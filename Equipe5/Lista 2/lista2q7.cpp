53219022acb1700df3e2ab93d00471a7e4a77514
alineelima <lima.alinee@gmail.com>
2016-08-25 17:57:02 -0400

    7

--- Equipe5/Lista 2/lista2q7.cpp
+++ Equipe5/Lista 2/lista2q7.cpp
@@ -8,40 +8,36 @@
 
 Questão 7 - Escreva uma função recursiva que determine quantas vezes um dígito K
 ocorre em um número natural N. Por exemplo, o dígito 2 ocorre 3 vezes em 762021192.*/
-
-#include <stdio.h> 
-
-int x;
-int digito=0;
-int k=0;
-int verifica_qt_digito(int n)
-{	
-	if(n == 0)
-    	return 0;
-    else
-    {
-    	if(digito == (n %10))
-    	{
-    		k++;
-    	}
-    	n = n / 10;
-    	return verifica_qt_digito(n);
-    }
-return 0;
-}
  
+ 
+#include<stdio.h>
+#include<stdlib.h>
+
+int x, cont=0;
+
+int repeticoes(int num)
+{
+	if(num==0)
+		return cont;
+	else
+	{
+		if(x==(num%10))
+		{
+			cont++;
+		}
+		return (repeticoes(num/10));
+	}
+}
+
 int main()
 {
-
-	printf("\nQuantidade de repeticao de um digito em um numero\n");
-	printf("\nDigite um numero:  ");
-   	scanf("%d",&x);
-   	printf("\nDigite o numero(digito) a ser verificado no numero informado:  ");
-   	scanf("%d",&digito);
-   	
-   	verifica_qt_digito(x);
-   	
-   	printf("\nQuantidade de vezes do digito informado repetido %d \n", k);
-	return 0;
+	int num;
+	
+	printf("Digite um numero: ");
+	scanf("%i", &num);
+	printf("\nInforme o numero que deseja verificar qunatas vezes se repete no N acima: ");
+	scanf("%i", &x);
+	
+	printf("\nO numero se repete %i vezes", repeticoes(num));	
 }
 
