93ca8d6777083e2e01b318f45fd543d153c2d51a
FernandoCalderaro <fernandosilvacalderaro@gmail.com>
2016-08-25 17:46:06 -0400

    subindo

--- Equipe1/Lista2/ex4_lista2.c
+++ Equipe1/Lista2/ex4_lista2.c
@@ -9,3 +9,59 @@
 	Dado um número n, gere todas as possíveis combinações com as n primeiras letras do alfabeto. Ex.: n = 3.
 	Resposta: ABC, ACB, BAC, BCA, CAB, CBA
 */
+
+#include<stdio.h>
+#include<string.h>
+
+
+imprimir(char *permutacao)
+{
+	printf("%s\n", permutacao);
+}
+
+void troca(char *a, char *b)
+{
+	char aux;
+	aux=*a;
+	*a=*b;
+	*b=aux;
+}
+
+
+void combinacao(int fim, int inicio, char *letra)
+{
+	int i;
+	if(inicio == fim)
+		imprimir(letra);
+	else
+	{
+		for(i=inicio;i<=fim;i++)
+		{
+			troca((letra+inicio),(letra+i));
+			combinacao(fim, inicio+1, letra);
+			
+		}
+	}
+}
+
+
+int main()
+{
+	char letras[]= {'A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'},*alfabeto;
+	int i, n;
+	
+	printf("Digite um numero: ");
+	scanf("%d", &n);
+	alfabeto=malloc(sizeof(char) * n);	
+	
+	for(i=0;i<n;i++)
+	{
+		alfabeto[i]=letras[i];
+	}
+	
+	alfabeto[n]='\0';
+	
+	combinacao(n-1, 0, alfabeto);
+	free(alfabeto);
+}
+
