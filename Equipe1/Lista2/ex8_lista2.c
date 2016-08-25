c50b6c5b46c1173453774b53d02852e6d50459ab
leticiaamota <leticiaamota@gmail.com>
2016-08-25 16:58:58 -0400

    correcao ex8

--- Equipe1/Lista2/ex8_lista2.c
+++ Equipe1/Lista2/ex8_lista2.c
@@ -1,4 +1,4 @@
-/* 	equipe 01:
+﻿/* 	equipe 01:
 	Diogo Abreu
 	Fernando Calderaro
 	Leticia Mota
@@ -7,42 +7,37 @@
 
 	exercicio 08
 	Uma sequência de Fibonacci generalizada, de f0 a f1 é definida como fibg(f0, f1, 0), fibg(f0, f1, 1), fibg(f0, f1, 2), ..., onde:
-	fibg(f0, f1, 0) = f0
-	fibg(f0, f1, 1) = f1
+	fibg(f0, f1, 1) = f0
+	fibg(f0, f1, 2) = f1
 	fibg(f0, f1, n) = fibg(f0, f1, n-1) + fibg(f0, f1, n-2), se n > 1.
 	Escreva uma função recursiva em C para calcular fibg(f0, f1, n).
 */
 
 #include <stdio.h>
 
-int fibg(int n)
+int fibg(int f0,int f1,int n)
 {
-	if (n==0)
-	{
-		return 0;
-	}
-	if (n==1)
-	{
-		return 1;
-	}
-	if (n>1)
-	{
-		return fibg(n-1)+fibg(n-2);
-	}
-	return 0;
+	int resultado;
+	if(n==0)
+		return f0;
+	if(n==1)
+		return f1;
+	if(n>1)
+		return fibg(f0,f1,n-1)+fibg(f0,f1,n-2);
 }
 
 int main()
 {
-	int i, n;
-
-	printf("-Programa que calcula e mostra uma Sequência de Fibonacci generalizada-\n\n");
-	printf("Digite um número:\n");
+	int f0,f1,n,i,resultado;
+	printf("insira valores para os limites f0 e f1: ");
+	scanf("%d %d",&f0,&f1);
+	printf("insira o tamanho da sequencia: ");
 	scanf("%d",&n);
-
-	for(i=0;i<=n;i++)
+	
+	for(i=0;i<n;i++)
 	{
-        printf("Sequência de Fibonacci generalizada: %d\n",fibg(i+1));
-	}
+		printf("%d ", fibg(f0,f1,i));
+	}		
+	
 	return 0;
 }
