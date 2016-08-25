4b128dea63ed1f55a57722853b41f89ff95a7e02
helioaze <helioaze@gmail.com>
2016-08-24 20:44:55 -0400

    questao3
    /*
     * =====================================================================================
     *
     *       Filename:  questão3.c
     *
     *    Description:  modelo de cabeçalho
     *
     *        Version:  1.0
     *        Created:  24-08-2016 20:44:12
     *       Revision:  none
     *       Compiler:  devc++
     *
     *         Author:  Hélio Paulo
     *         			Jailson Pereira
     *         			Tiago Cauassa
     *         			Rebeca Assunção
     *   Organization:  
     *
     * =====================================================================================

---
+++ Equipe6/lista2/questão3.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int soma( int a, int b)
+
+{
+
+int i,soma ;
+
+ for (i=0;i<b;i++)
+ {
+
+	soma = ++ a;
+}  
+	return (soma);
+}
+
+
+main()
+
+{
+int soma( int a, int b);
+int n1,n2,resultado;
+
+
+printf("Digite um numero: ");
+scanf("%d",&n1);
+
+printf("Digite outro numero: ");
+scanf("%d",&n2);
+
+resultado = soma ( n1, n2);
+
+    
+printf("A soma eh: %d ", resultado) ;
+
+return 0;
+}
