687267df71ec49e6a6d785345b9efbc168d1aa9a
Lucas <lucas.alsilva01@gmail.com>
2016-08-25 18:23:08 -0400

    Questao5

---
+++ Equipe5/Lista 2/lista2q5.cpp
@@ -0,0 +1,35 @@
+#include<stdio.h>
+void FindNext(int,int,int,int,int,int);
+
+int main(){
+	FindNext(0,0,0,0,0,0);
+	
+}
+void FindNext(int var1,int var2,int var3,int var4,int var5,int var6){
+	var1++;
+	printf("%d %d %d %d %d %d\n",var1,var2,var3,var4,var5,var6);
+	if(var1 >99){
+		var1=0;
+		var2++;
+	}
+		if(var2 >99){
+		var2=0;
+		var3++;
+	}
+		if(var3 >99){
+		var3=0;
+		var4++;
+	}
+		if(var4 >99){
+		var4=0;
+		var5++;
+	}
+		if(var5 >99){
+		var5=0;
+		var6++;
+	}
+		if(var6 >99){
+		return;
+	}
+	FindNext(var1,var2,var3,var4,var5,var6);
+}
