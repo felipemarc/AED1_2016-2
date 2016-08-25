34b3217832e35fc35ae5e7ff7a6e105b1152d04b
bigustorm <paulo.roberto.uea@gmail.com>
2016-08-25 17:13:56 -0400

    Código pronto

--- equipe7/Lista_02/Exe07.c
+++ equipe7/Lista_02/Exe07.c
@@ -5,7 +5,8 @@
     Pedro Sena
     Sywan Neto
 
-	7) Escreva uma função recursiva que determine quantas vezes um dígito K ocorre em um número natural N. Por exemplo, o dígito 2 ocorre 3 vezes em 762021192.    
+	7) Escreva uma função recursiva que determine quantas vezes um dígito K ocorre em um número natural N.
+	Por exemplo, o dígito 2 ocorre 3 vezes em 762021192.    
 */
 #include<stdio.h>
 #include <string.h>
