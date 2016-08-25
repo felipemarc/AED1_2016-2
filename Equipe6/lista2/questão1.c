532333222aaae11abebe3955f154702db0ab6d90
Rebeca Assunção <rsva.lic@uea.edu.br>
2016-08-25 10:44:32 -0400

    questão concluída

--- Equipe6/lista2/questão1.c
+++ Equipe6/lista2/questão1.c
@@ -1,3 +1,24 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  questão1.c
+ *
+ *    Description:  questão1.c
+ *
+ *        Version:  1.0
+ *        Created:  25-08-2016 10:43:12
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  		Hélio Paulo
+ *         			Jailson Pereira
+ *         			Tiago Cauassa
+ *         			Rebeca Assunção
+ *   Organization:  
+ *
+ * =====================================================================================
+ */
+ //1) Impressão de um número natural em base binária
 #include <stdio.h>
 #include <stdlib.h>
 
@@ -10,6 +31,7 @@ int main() {
 	scanf("%d", &n);
 	
     binario(n);
+    
     return 0;
 }
 void binario(int n)
