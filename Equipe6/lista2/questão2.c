9fbba89889d23404bcc496e12f1491a91c6c5d56
Rebeca Assunção <rsva.lic@uea.edu.br>
2016-08-25 11:35:25 -0400

    questão concluída

--- Equipe6/lista2/questão2.c
+++ Equipe6/lista2/questão2.c
@@ -20,5 +20,29 @@
  */
  //2) Multiplicação de dois números naturais, através de somas sucessivas (Ex.: 6 ∗ 4 = 4+4+4+4+4+4)
  
- 
- 
+#include <stdio.h>
+#include <stdlib.h>
+
+int multi (int,int);
+
+int main(){
+	int x, y;
+	
+	printf("informe x:");
+	scanf("%d",&x);
+	printf("informe :");
+	scanf("%d",&y);
+	
+	printf("multiplicacao por somas sucessivas: %d", multi(x,y));
+	
+	return 0;
+}
+
+int multi(int x, int y){
+	
+	if(x==0){
+		return 0;
+	}
+	return y + multi(x-1, y);
+	
+}
