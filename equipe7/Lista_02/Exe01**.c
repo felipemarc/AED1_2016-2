f90735afa2afc1c2d1594dfeb60294e6df8e0073
Paulo90 <psj.jennings@hotmail.com>
2016-08-26 12:45:16 -0400

    Create Exe01**.c

---
+++ equipe7/Lista_02/Exe01**.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+void convert_binario(int n)
+{
+    if (n / 2 != 0)
+    {
+        convert_binario(n / 2);
+    }
+    printf("%d", n % 2);
+}
+int main()
+{
+    int n, x;
+    scanf("%d", &n);
+    printf("\n");
+    printf("Forma binaria: ");
+    printf("\n");
+    convert_binario(n);
+    printf("\n");
+    
+    return 0;
+}
