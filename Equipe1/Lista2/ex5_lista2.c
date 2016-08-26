731391995da6a2e7a638b662dd137a5433ff100d
leticiaamota <leticiaamota@gmail.com>
2016-08-26 15:20:32 -0400

    questao 5

--- Equipe1/Lista2/ex5_lista2.c
+++ Equipe1/Lista2/ex5_lista2.c
@@ -1,4 +1,4 @@
-/* 	equipe 01:
+﻿/* equipe 01:
 	Diogo Abreu
 	Fernando Calderaro
 	Leticia Mota
@@ -6,5 +6,46 @@
 	Thailana Neves		
 	
 	exercicio 05
-	Gere todas as possíveis combinações para um jogo da MegaSena com 6 dezenas.	
+	
+	Gere todas as possíveis combinações para um jogo da MegaSena com 6 dezenas. 50.063.860
+
 */
+
+#include <stdio.h>
+
+int seq[100000];
+
+void comb(int beg, int n, int k)
+{
+        int i;
+        if(k == 1)
+        {
+                seq[k-1] = beg;
+                for(i=n;i>=0;i--)
+                { 
+                        if(seq[i]!=0)
+                        {
+                             printf("%d ",seq[i]);
+                        }
+                }
+                printf("\n");
+        }
+        
+        for(i = beg+1;i <= n;i++)
+        {
+                seq[k-1] = beg;
+                comb(i,n, k-1);
+        }
+}
+
+int main()
+{
+        int i,n,k;
+        scanf("%d%d",&n,&k);
+
+        for(i = 1;i <= n-1;i++)
+        {
+                 comb(i,n,k);
+        }
+        return 0;
+}
