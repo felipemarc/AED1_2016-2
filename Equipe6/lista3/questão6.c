45d14b909fdfd331c697afdb7d3c8ec72b6ed990
Rebeca Assunção <rsva.lic@uea.edu.br>
2016-09-03 19:54:47 -0400

    Create questão6.c

---
+++ Equipe6/lista3/questão6.c
@@ -0,0 +1,21 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  questão6.c
+ *
+ *    Description:  questão6
+ *
+ *        Version:  1.0
+ *        Created:  03-09-2016 19:55:35
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  Hélio Paulo
+ *         			Jailson Pereira
+ *         			Tiago Cauassa
+ *         			Rebeca Assunção
+ *   Organization:  AED 1
+ *
+ * =====================================================================================
+ */
+ //6) Implemente a Busca Binária em C
