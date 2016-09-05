c7f7e70c28f5ef95b2328138a05a10c0b9cf1d32
helioaze <helioaze@gmail.com>
2016-09-05 10:47:50 -0400

    Update questao2.c

--- Equipe6/lista3/questao2.c
+++ Equipe6/lista3/questao2.c
@@ -6,7 +6,7 @@
  *    Description:  
  *
  *        Version:  1.0
- *        Created:  05-09-2016 10:13:20
+ *        Created:  05-09-2016 10:46:20
  *       Revision:  none
  *       Compiler:  gcc
  *
@@ -37,4 +37,31 @@ int main(){
 		printf("%d ", vetor[i]);
 	}
 	printf("\n");	
+}
+void Quick(int vetor[10], int inicio, int fim){
+	
+	int pivo, aux, i, j, meio;
+	
+	i = inicio;
+	j = fim;
+	
+	meio = (int) ((i + j) / 2);
+	pivo = vetor[meio];
+	
+	do{
+        	while (vetor[i] < pivo) i = i + 1;
+ 		while (vetor[j] > pivo) j = j - 1;
+	
+		if(i <= j){
+			aux = vetor[i];
+			vetor[i] = vetor[j];
+			vetor[j] = aux;
+			i = i + 1;
+			j = j - 1;
+		}
+	}while(j > i);
+	
+	if(inicio < j) Quick(vetor, inicio, j);
+	if(i < fim) Quick(vetor, i, fim);	
+
 }
