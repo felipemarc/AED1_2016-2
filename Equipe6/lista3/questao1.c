f92f759802bfe1b3b1562fbad2cd6a541c6f18f6
helioaze <helioaze@gmail.com>
2016-09-05 09:22:15 -0400

    Update_questao1.c
    faltando o arquivo texto

--- Equipe6/lista3/questao1.c
+++ Equipe6/lista3/questao1.c
@@ -16,3 +16,47 @@
  *
  * =====================================================================================
  */
+#include <stdio.h>
+#include <stdlib.h>
+
+
+void bubble (int vetor[], int n)
+{
+
+int i,j;
+int k = n ;
+int aux;
+	for(i=0;i<n;i++){
+		for (j=0;j<k;j++){
+			if (vetor[j] > vetor [j+1]){
+				aux = vetor [j];
+				vetor[j] = vetor[j+1];
+				vetor [j+1] = aux;
+			}
+		}
+
+		k--;
+	}
+
+}
+
+
+
+
+int main ()
+{
+
+int vetor [] = {3,5,8,1,9,2,4,7,0,6};
+int n = 10 ;
+int i;
+
+
+bubble (vetor,n);
+printf("\n\n\n");
+
+	for(i=0;i<n;i++){
+		printf("%d - ", vetor[i]);
+
+	}
+
+}
