0e9e7fff0180b1d59a4aba1b406d84b9d8d2cbca
jailsonpj <jailsoncolares@gmail.com>
2016-09-05 21:43:44 -0400

    trabalhando na questao

--- Equipe6/lista3/questao5.c
+++ Equipe6/lista3/questao5.c
@@ -18,61 +18,67 @@
 
 #include<stdio.h>
 #include<stdlib.h>
-
+#define tam 10
 int busca_seq (int vet[],int k){
 	int i;
-	const int tam = 10;
+//	const int tam = 10;
 	for(i=0;i<tam;i++){
 		if(vet[i] == k){
-			return vet[i];
+			return i;
 		}
 		else{
-			return NULL;
+			return -1;
 		}
 	}
 }
 
 void print_arquivo(int vet[]){
 	int i;
-	const int tam = 10;
-	for(i=0;i<tam;i++)
+//	const int tam = 10;
+	for(i=0;i<=tam-1;i++)
 	{
 		printf("%d\n",vet[i]);
 	}
 }
 
+void load (int vet[]){
+	int i;
+	int ch;
+	FILE *arq;
+	arq = fopen("teste.txt","r");
+	if(arq){
+		do{
+			for(i=0;i<tam;i++){
+				fscanf(arq,"%d",&vet[i]);
+			}
+		}while( !feof (arq));
+	}
+}
+
 int main(int argc,const char *argv[]){
 
-		//char url[] = "teste.txt";
-		/*  char ch;
-		FILE *arq;*/
-		const int tam = 10;
-		int i,k,num;
-		int vet[tam];	
-		for(i=0;i<tam;i++){
-			scanf("%d",&vet[i]);
-		}
-		print_arquivo(vet);
-		printf("#########################################");
-		printf("digite numero a pesquisar: ");
-		scanf("%d",&k);
-		printf("#########################################");
-		num = busca_seq(vet,k);
-		printf("a posicao do numero está em: %d\n",num);
-			/*  arq = fopen(url,"r");
-		if( arq == NULL){
-			printf("Erro ao abrir arquivo\n");
-		}
-		else{
-			while((ch=fgetc(arq)) != EOF){
-			
-				putchar(ch);
-				
-			}
-		}
 		
-		fclose(arq);*/
+	//	const int tam = 10;
+		int i,num;
+		int k;
+		int vet[tam];	
+		/*  for(i=0;i<=tam-1;i++){
+			scanf("%d",&vet[i]);
+		}*/
+			
+		load(vet);
+		print_arquivo(vet);
+		printf("#########################################\n");
+		
+	    printf("digite numero a pesquisar:");
+		scanf("%d",&k);
+		
+		//printf("\n#########################################\n");
+		//num = busca_seq(vet,k);
+		
+		//printf("a posicao do numero está em: %d\n",num);
 
+		
 	return 0;
 }
 
