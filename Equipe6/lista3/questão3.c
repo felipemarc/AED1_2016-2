8cdc634d3a983816ebfbabe15e80f2aa5a375f71
Cauassa <tiagocauassa@gmail.com>
2016-09-06 14:29:23 -0400

    Atualizando

--- Equipe6/lista3/questão3.c
+++ Equipe6/lista3/questão3.c
@@ -1,3 +1,15 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  modelo.c
+ *
+ *    Description:  modelo de cabeçalho
+ *
+ *        Version:  1.0
+ *        Created:  19-08-2016 14:07:12
+ *       Revision:  none
+
+
 #include <stdio.h>
 #include <stdlib.h>
   
