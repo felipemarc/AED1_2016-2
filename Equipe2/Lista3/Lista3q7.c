3dc03185b98ceacd11fffccfe403aa15e11c61b0
jessiccatavares <jldst.snf@uea.edu.br>
2016-09-08 22:06:48 -0300

    Create Lista3q7.c

---
+++ Equipe2/Lista3/Lista3q7.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+#define tam 10
+int sentinela(int numero, int chave, int vec[])
+{
+	int i=0;
+	vec[tam]=chave;
+	
+	while(chave!=vec[i])
+	{
+		i++;
+	}
+	
+	if(i<numero)
+	{
+		return i;
+	}
+		
+	return -1;
+}
+
+
+
+int main()
+{
+	int vec[tam+1]={9,23,56,3,25,78,81,3,6,34}, numero;
+	
+	//printf("Informe um numero: "); scanf("%d",&numero);
+	printf("%d",sentinela(5, tam, vec));
+	
+	system("pause");
+}
