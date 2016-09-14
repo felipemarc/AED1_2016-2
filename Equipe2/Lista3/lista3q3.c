b455e840a2151b5b26a0d799ace99307b7d75c02
Botinelly <lucas_botinelly@hotmail.com>
2016-09-14 18:19:19 -0400

    att

--- Equipe2/Lista3/lista3q3.c
+++ Equipe2/Lista3/lista3q3.c
@@ -7,42 +7,82 @@ Paulo Marinho
 Stephanny Barreto
 
 3) Crie um programa em C que gere em um arquivo .txt
-- arquivo com 10000 números ordenados em ordem crescente
-- arquivo com 10000 números ordenados em ordem descrescente
-- arquivo com 10000 números gerados de maneira aleatória
-- arquivo com 100000 números ordenados em ordem crescente
-- arquivo com 100000 números ordenados em ordem descrescente
-- arquivo com 1000000 números gerados de maneira aleatória
-- arquivo com 1000000 números ordenados em ordem crescente
-- arquivo com 1000000 números ordenados em ordem descrescente
-- arquivo com 1000000 números gerados de maneira aleatória
-*/
-/*Compilar assim : 
+
+1 arquivo com 10000 números ordenados em ordem crescente
+
+2 arquivo com 10000 números ordenados em ordem descrescente
+
+3 arquivo com 10000 números gerados de maneira aleatória
+
+4 arquivo com 100000 números ordenados em ordem crescente
+
+5 arquivo com 100000 números ordenados em ordem descrescente
+
+6 arquivo com 1000000 números gerados de maneira aleatória
+
+7 arquivo com 1000000 números ordenados em ordem crescente
+
+8 arquivo com 1000000 números ordenados em ordem descrescente
+
+9 arquivo com 1000000 números gerados de maneira aleatória
+
+Compilar assim : 
 gcc lista3q3.c -o /q3
 ./q3 > saida.txt	
 */
 #include<stdio.h>
 #include<stdlib.h>
 #include<time.h>
-#include<lista3q1.c>
 
-void gera(int opc)
+void gera(char opc)
 {
-	int i =0;
-	if(opc == 1)
+	int i;
+	switch(opc)
 	{
-		for(i = 0; i<10000; i++)
-			printf("%d\n", i);
+		case '1':
+			for(i=0; i<10000; i++)
+				printf("%d ", i);
+			break;
+		case '2':
+			for(i=10000; i>0; i--)
+				printf("%d ", i);
+			break;
+		case '3':
+			for(i=0; i<1000; i++)
+				printf("%d ", rand() % 100);
+			break;
+		case '4':
+			for(i=0; i<100000; i++)
+				printf("%d ", i);
+			break;
+		case '5':
+			for(i=100000; i>0; i--)
+				printf("%d ", i);
+			break;
+		case '6':
+			for(i=0; i<10000; i++)
+				printf("%d ", rand() % 100);
+			break;
+		case '7':
+			for(i=0; i<100000; i++)
+				printf("%d ", i);
+			break;
+		case '8':
+			for(i=1000000; i>0; i--)
+				printf("%d ", i);
+			break;
+		case '9':
+			for(i=0; i<100000; i++)
+				printf("%d ", rand() % 100);
+			break;			
 	}
-	if(opc == 2)
-	{
-		for(i = 10000; i>0; i--)
-			printf("%d\n", i);	
-	}
-	if(opc == 3)
-	{
-		for(i = 0; i>100000; i--)
-		{
-		}	
-	}
+}
+
+int main()
+{
+	char opc;
+	scanf("%c", &opc);
+	gera(opc);
+	printf("\n");
+	return 0;
 }
