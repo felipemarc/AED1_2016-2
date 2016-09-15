87067582f401e7e8434d497d2e9311c7172a7f15
Cauassa <tiagocauassa@gmail.com>
2016-09-15 12:31:34 -0300

    Preencher

---
+++ Equipe6/lista3/questão8.c
@@ -0,0 +1,21 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  busca.c
+ *
+ *    Description:  Busca Sequencial com Sentinela
+ *
+ *        Version:  1.0
+ *        Created:  03-09-2016 20:49:52
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:                  Hélio Paulo
+ *         			    Jailson Pereira
+ *         			    Tiago Cauassa
+ *         			    Rebeca Assunção
+ *         			    Miguel Angelo
+ *   Organization:  AED1
+ *
+ * =====================================================================================
+ */
