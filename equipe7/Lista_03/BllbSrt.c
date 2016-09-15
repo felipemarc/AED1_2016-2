60653cfe412f210dbfd3df6a9fbf983a3ade37e5
fabianabezerra <fabisouza.bezerra@gmail.com>
2016-09-15 18:52:40 -0400

    Update BllbSrt.c

--- equipe7/Lista_03/BllbSrt.c
+++ equipe7/Lista_03/BllbSrt.c
@@ -1,35 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-
-
-int main(int argc, char *argv[])
-{
-
-    time_t tinicio, tfim;
-
-    tinicio = time(NULL);
-    
-    int max = atoi(argv[1]);
-    
-    int tempo, i, vetor [max];
-
-
-    for (i = 0;i < MAX; i++)
-    {
-        scanf("%d",&vetor[i])
-    }
-
-    bubble(vetor[], MAX);
-
-    tfim = time(NULL);
-    tempo = difftime(tinicio, tfim);
-    printf("Tempo de execução: %d", tempo);
-    return 0;
-}
-
-//C:\teste>BllbSrt 10000 < 10000c.txt
-
 void bubble(int vetor[], int tam)
 {
     int i, aux;
@@ -42,5 +13,33 @@ void bubble(int vetor[], int tam)
                 vetor[i+1] = aux;
             }
         }
-    }
 }
+
+int main(int argc, const char *argv[])
+{
+
+    time_t tinicio, tfim;
+
+    tinicio = time(NULL);
+    
+    int max = atoi(argv[1]);
+    
+    int i, vetor [max];
+
+    float tempo; 
+
+    for (i = 0;i < max; i++)
+    {
+        scanf("%d",&vetor[i]);
+    }
+
+    bubble(vetor, max);
+
+    tfim = time(NULL);
+    tempo = difftime(tfim, tinicio);
+    printf("Tempo de execução: %d", tempo);
+    return 0;
+}
+
+//C:\teste>BllbSrt 10000 < 10000c.txt
+
