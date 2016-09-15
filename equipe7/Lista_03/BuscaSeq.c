4ebbf902847b929ebd36cdd35ec0b2c48d2e7831
bigustorm <paulo.roberto.uea@gmail.com>
2016-09-15 16:33:39 -0400

    Código pronto

--- equipe7/Lista_03/BuscaSeq.c
+++ equipe7/Lista_03/BuscaSeq.c
@@ -1,1 +1,46 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+
+
+int main(int argc, char *argv[])
+{
+
+    time_t tinicio, tfim;
+
+    tinicio = time(NULL);
+
+    int max = atoi(argv[1]);
+    int chave = atoi(argv[2]);
+
+    int i, vetor[max];
+
+    for (i=0;i < max;i++) scanf("%d", vetor[i]);
+
+    busca_seq(vetor, chave);
+
+    tfim = time(NULL);
+    tempo = difftime(tinicio, tfim);
+    printf("Tempo de execução: %d segundos", tempo);
+
+    return 0;
+}
+
+void busca_seq (int vetor[], int chave)
+{
+    int i;
+
+    for (i=0;i<n;i++)
+    {
+        if (vetor[i]==chave)
+        {
+            printf("Chave encontrada \n");
+            break;
+        }
+    }
+
+    printf("Chave não encontrada \n");
+    break;
+}
 
