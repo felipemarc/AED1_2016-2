804532c752ced23de7f7aecbc6703e3a7a5fe87c
miguelrodrigs <miguelcazemiro@gmail.com>
2016-09-14 20:45:06 -0400

    Confirmar Conclusão

--- Equipe6/lista3/questão7.c
+++ Equipe6/lista3/questão7.c
@@ -3,7 +3,7 @@
  *
  *       Filename:  busca.c
  *
- *    Description:  busca
+ *    Description:  Busca Sequencial com Sentinela
  *
  *        Version:  1.0
  *        Created:  03-09-2016 20:49:52
