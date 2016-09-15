e6a8cad6f0d2568d70ee164522b4f2dbbbf9c87d
miguelrodrigs <miguelcazemiro@gmail.com>
2016-09-14 20:54:37 -0400

    Confirmar Conclusão

--- Equipe6/lista3/questão7.c
+++ Equipe6/lista3/questão7.c
@@ -10,7 +10,11 @@
  *       Revision:  none
  *       Compiler:  gcc
  *
- *         Author:  YOUR NAME (), 
+ *         Author:                  Hélio Paulo
+ *         			    Jailson Pereira
+ *         			    Tiago Cauassa
+ *         			    Rebeca Assunção
+ *         			    Miguel Angelo
  *   Organization:  AED1
  *
  * =====================================================================================
