00cd4a69b4ccf949c7404ccf4cd4490c770292cd
Botinelly <lucas_botinelly@hotmail.com>
2016-09-15 15:51:55 -0400

    att

--- Equipe2/Lista3/gerador.c
+++ Equipe2/Lista3/gerador.c
@@ -3,34 +3,43 @@
 
 void gera(int tam, char tipo)
 {
+  int i;
 	if(tipo == 'c' || tipo == 'C')
 	{
+	
 		printf("%d", tam);
 		printf("\n");
+		
 		for(i=0; i<tam; i++)
 			printf("%d\n", i);
 	}
-	if(tipo == 'D' || tipo == 'd')
+	else if(tipo == 'D' || tipo == 'd')
 	{
+	
 		printf("%d", tam);
 		printf("\n");
+		
 		for(i=tam; i>0; i--)
 			printf("%d\n", i);
 	}
-	if(tipo == 'A' || tipo == 'a')
+	else if(tipo == 'A' || tipo == 'a')
 	{
+	
 		printf("%d", tam);
 		printf("\n");
+		
 		for(i=0; i<tam; i++)
 			printf("%d\n", rand() % 10000);
 	}			
+  else
+    printf("Argumento Inválido! \n");
 }	
-int main()
-{
-	int tam;
-	char tipo;
-	scanf("%d", &tam);
-	scanf("%c", &tipo);
-	gera(tam, tipo);
+
+
+int main(int argc, const char *argv[])
+{	
+	
+	gera(atoi(argv[1]), argv[2][0]);
+	
 	return 0;
 }
