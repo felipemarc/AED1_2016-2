eb0be628ae970ba97fdd02083db3699dbf1ef2cb
Botinelly <lucas_botinelly@hotmail.com>
2016-09-15 15:12:58 -0400

    Gerador Pronto

--- equipe7/Lista_03/Gerador.c
+++ equipe7/Lista_03/Gerador.c
@@ -1,1 +1,25 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <time.h>
 
+int main (int argc, char *argv[])
+{
+	int MAX = atoi(argv[1]);
+	int i;
+	
+	srand(time(NULL));
+	
+	
+	if(strcmp(argv[2], "D"))
+	{
+		for(i = MAX-1; i>0;i--) printf("%d ", i);
+	}
+	else
+	if(strcmp(argv[2], "A")
+	{
+		for(i = 0; i < MAX; i++) printf("%d ", rand()%1000000);
+	}
+	else for(i = 0; i < MAX; i++) printf("%d ", i);
+	
+}
