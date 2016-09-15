da87bdfb7e22c2f3513c6ffdbf0d69129c5a10d9
Paulo90 <psj.jennings@hotmail.com>
2016-09-15 15:25:55 -0400

    Update BllbSrt.c

--- equipe7/Lista_03/BllbSrt.c
+++ equipe7/Lista_03/BllbSrt.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <time.h>
 
 
@@ -8,27 +9,26 @@ int main(int argc, char *argv[])
     time_t tinicio, tfim;
 
     tinicio = time(NULL);
+    
+    int max = atoi(argv[1]);
+    
+    int tempo, i, vetor [max];
 
-    MAX = (int)argv[1];
-
-    int tempo, i, vetor [MAX];
-
-
-    srand(time(NULL));
 
     for (i = 0;i < MAX; i++)
     {
-        vetor[i] = rand() % 1000000;
+        scanf("%d",&vetor[i])
     }
 
     bubble(vetor[], MAX);
 
     tfim = time(NULL);
     tempo = difftime(tinicio, tfim);
+    printf("Tempo de execução: %d", tempo);
     return 0;
 }
 
-//C:\teste>BllbSrt 10000
+//C:\teste>BllbSrt 10000 < 10000c.txt
 
 void bubble(int vetor[], int tam)
 {
