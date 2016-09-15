5b5cac188356021a3545706432ec3983755fb6ba
miguelrodrigs <miguelcazemiro@gmail.com>
2016-09-14 20:50:16 -0400

    Confirmar Conclusão

--- Equipe6/lista3/questão3.c
+++ Equipe6/lista3/questão3.c
@@ -10,7 +10,7 @@
  *       Revision:  none
  *       Compiler:  gcc
  *
- *         Author:  Hélio Paulo
+ *         Author:Hélio Paulo
  *         			    Jailson Pereira
  *         			    Tiago Cauassa
  *         			    Rebeca Assunção
