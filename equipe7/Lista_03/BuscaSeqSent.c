8952c3ba91dc40ec70634f822b3bfa56909eecf4
fabianabezerra <fabisouza.bezerra@gmail.com>
2016-09-15 19:08:40 -0400

    Update BuscaSeqSent.c

--- equipe7/Lista_03/BuscaSeqSent.c
+++ equipe7/Lista_03/BuscaSeqSent.c
@@ -3,44 +3,45 @@
 #include <time.h>
 
 
-
-int main(int argc, char *argv[])
-{
-
-    time_t tinicio, tfim;
-
-    tinicio = time(NULL);
-
-    int max = atoi(argv[1]);
-    int chave = atoi(argv[2]);
-
-    int i, vetor[max];
-
-    for (i=0;i < max;i++) scanf("%d", vetor[i]);
-
-    busca_seq_sent(vetor, chave, max);
-
-    tfim = time(NULL);
-    tempo = difftime(tinicio, tfim);
-    printf("Tempo de execução: %d segundos", tempo);
-
-    return 0;
-}
-
-void busca_seq_sent (int vetor[], int chave, int tam)
+int busca_seq_sent (int vetor[], int chave, int tam)
 {
     int i, sent = -1;
 
-    vetor[max] = sent;
+    vetor[tam] = sent;
 
     for (i=0;sent!=vetor[i];i++);
 
     if (i<tam)
     {
         printf("Chave encontrada \n");
-        break
+        return 1;
     }
     printf("Chave não encontrada \n");
-    break;
+    return 0;
 }
 
+
+int main(int argc, const char *argv[])
+{
+
+    time_t tinicio, tfim;
+
+    tinicio = time(NULL);
+    
+    float tempo;
+
+    int max = atoi(argv[1]);
+    int chave = atoi(argv[2]);
+
+    int encontrou, i, vetor[max];
+
+    for (i=0;i < max;i++) scanf("%d", &vetor[i]);
+
+    encontrou = busca_seq_sent(vetor, chave, max);
+
+    tfim = time(NULL);
+    tempo = difftime(tfim, tinicio);
+    printf("Tempo de execução: %f segundos", tempo);
+
+    return 0;
+}
