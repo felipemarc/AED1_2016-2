6097675dc491223f37085ccd6d7211b6fda385cc
fabianabezerra <fabisouza.bezerra@gmail.com>
2016-09-15 19:06:18 -0400

    Update BuscaBin.c

--- equipe7/Lista_03/BuscaBin.c
+++ equipe7/Lista_03/BuscaBin.c
@@ -2,34 +2,10 @@
 #include <stdlib.h>
 #include <time.h>
 
-
-
-int main(int argc, char *argv[])
-{
-
-    time_t tinicio, tfim;
-
-    tinicio = time(NULL);
-
-    int max = atoi(argv[1]);
-    int chave = atoi(argv[2]);
-
-    int i, vetor[max];
-
-    for (i=0;i < max;i++) scanf("%d", vetor[i]);
-
-    busca_bin(vetor, chave, max);
-
-    tfim = time(NULL);
-    tempo = difftime(tinicio, tfim);
-    printf("Tempo de execução: %d segundos", tempo);
-
-    return 0;
-}
-
-void busca_bin (int vetor[], int chave, int tam)
+int busca_bin (int vetor[], int chave, int tam)
 {
     int i, j, k;
+    int x = 0;
     i = 0; k = tam - 1;
 
     while(i <= k)
@@ -37,15 +13,44 @@ void busca_bin (int vetor[], int chave, int tam)
         j = (i + k)/2;
         if(vetor[j] == chave)
         {
+            x = 1;
             printf ("chave encontrada");
-            break;
+            
         }
         if(vetor[j] < chave) i = j + 1;
         else k = j - 1;
     }
-
-    printf("chave não encontrada");
-    break;
+    if (x==0)
+    {
+     printf("chave não encontrada");
+    }
+     return 0;
 
 }
+
+int main(int argc, const char *argv[])
+{
+
+    time_t tinicio, tfim;
+
+    tinicio = time(NULL);
+    float tempo;
+    
+    int max = atoi(argv[1]);
+    int chave = atoi(argv[2]);
+
+
+    int try, i, vetor[max];
+
+    for (i=0;i < max;i++) scanf("%d", &vetor[i]);
+
+    try = busca_bin(vetor, chave, max);
+
+    tfim = time(NULL);
+    tempo = difftime(tfim, tinicio);
+    printf("Tempo de execução: %f segundos", tempo);
+
+    return 0;
+}
+
 
