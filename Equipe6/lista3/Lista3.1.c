aac149986f7014ca31644e3be5a2564d10b5c10f
jailsonpj <jailsoncolares@gmail.com>
2016-09-16 01:20:31 -0400

    atualizando questao

--- Equipe6/lista3/Lista3.1.c
+++ Equipe6/lista3/Lista3.1.c
@@ -29,9 +29,12 @@ void bubble (int vet[],int tam)
 
 	int i,j;
 	int aux;
-	for(i=0;i<tam;i++){
-		for (j=i+1;j<tam;j++){
-			if (vet[i] > vet [j]){
+	for(i=0;i<tam;i++)
+	{
+		for (j=i+1;j<tam;j++)
+		{
+			if (vet[i] > vet [j])
+			{
 				aux = vet [i];
 				vet[i] = vet[j];
 				vet [j] = aux;
@@ -54,7 +57,8 @@ int main (int argc,const char *argv[])
 	
 	int *vet = (int *) malloc (sizeof (int)*tam);
 
-	for (i=0;i<tam;i++){
+	for (i=0;i<tam;i++)
+	{
 		scanf("%d",&vet[i]);
 	}
 
