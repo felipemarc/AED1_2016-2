e0e6dd048f4f7134626a566d2c75918e43ff2d86
diogo1992 <dias.eng@uea.edu.br>
2016-10-03 09:06:43 -0300

    criando pasta

---
+++ Equipe1/Lista4/ex1_lista4.c
@@ -0,0 +1,15 @@
+/* 	Equipe 01:
+	Diogo Abreu
+	Elisandra Oliveira
+	Fernando Calderaro
+	Leticia Mota
+	Luana Andrade
+	Rodrigo Oliveira
+	Thailana Neves
+
+	Exercicio 01
+	Crie uma agenda em forma de lista duplamente encadeada.
+        - CRUD completo: inserir, deletar, atualizar, mostrar e listar
+        - dados nome, endereço, telefone e email
+        - a chave de busca será o número de telefone ( apenas números )
+*/
