a1c0c6e0ad1bddbed8da8d14aea670293454f00f
jailsonpj <jailsoncolares@gmail.com>
2016-10-03 11:13:52 -0400

    atualizando questao

--- Equipe6/lista4/lista4.1/lista4.1.c
+++ Equipe6/lista4/lista4.1/lista4.1.c
@@ -6,10 +6,12 @@
 int main(int argc, const char * argv[]) 
 {
     Lista lista;
-    lista = create();
+    create(&lista);
     
-   // Item it1("CC001", "Poder mental", 90);
-    //lista.insere(it1);
+    Item it1 = {90};
+    insert(&lista,it1);
+    
+    print_lista(&lista);
     
   return 0;
 } 
