0c12b02ddcd6cdfb5b47d74eddbd583f02b42dcf
jailsonpj <jailsoncolares@gmail.com>
2016-10-03 11:00:53 -0400

    adicionando main da questao

---
+++ Equipe6/lista4/lista4.1/lista4.1.c
@@ -0,0 +1,15 @@
+
+
+#include <stdio.h>
+#include "lista4.1.h"
+
+int main(int argc, const char * argv[]) 
+{
+    Lista lista;
+    lista = create();
+    
+   // Item it1("CC001", "Poder mental", 90);
+    //lista.insere(it1);
+    
+  return 0;
+} 
