7207d323e392a9d35df0d47d9700d60cd556e928
jailsonpj <jailsoncolares@gmail.com>
2016-10-04 22:47:12 -0400

    atualizando questao

--- Equipe6/lista4/lista4.1/lista4.1.c
+++ Equipe6/lista4/lista4.1/lista4.1.c
@@ -7,11 +7,35 @@ int main(int argc, const char * argv[])
 {
     Lista lista;
     create(&lista);
+    int escolha;
     
-    Item it1 = {90};
-    insert(&lista,it1);
+    printf("--------Agenda AED1---------\n");
+    printf("    1 - Iserir contato     -\n");
+    printf("    2 - Mostrar Contato    -\n");
+    printf("    3 - Remover Contato    -\n");
+    printf("    4 - Atualizar Contato  -\n");
+    printf("    5 - Sair               -\n");
+    printf("----------------------------\n");
+    scanf("%d",&escolha);
+    switch(escolha){
+        case 1 :
+            break;
+        case 2 :
+            break;
+        case 3 :
+            break;
+        case 4 :
+            break;
+        case 5 :
+            break;
+        default:
+            printf("opção Inválida\n");
+    }
     
-    print_lista(&lista);
+    //Item it1 = {90};
+    //insert(&lista,it1);
+    
+   // print_lista(&lista);
     
   return 0;
 } 
