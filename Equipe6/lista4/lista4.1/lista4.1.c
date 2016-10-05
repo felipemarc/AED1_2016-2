d6a62444fa33c358a0c9a0fad80802d23e521254
jailsonpj <jailsoncolares@gmail.com>
2016-10-05 01:08:12 -0400

    atualizando questao

--- Equipe6/lista4/lista4.1/lista4.1.c
+++ Equipe6/lista4/lista4.1/lista4.1.c
@@ -8,30 +8,48 @@ int main(int argc, const char * argv[])
     Lista lista;
     create(&lista);
     int escolha;
+   do
+   {
+        printf("--------Agenda AED1---------\n");
+        printf("    1 - Iserir contato     -\n");
+        printf("    2 - Mostrar Contato    -\n");
+        printf("    3 - Remover Contato    -\n");
+        printf("    4 - Atualizar Contato  -\n");
+        printf("    5 - Sair               -\n");
+        printf("----------------------------\n");
+        scanf("%d",&escolha);
     
-    printf("--------Agenda AED1---------\n");
-    printf("    1 - Iserir contato     -\n");
-    printf("    2 - Mostrar Contato    -\n");
-    printf("    3 - Remover Contato    -\n");
-    printf("    4 - Atualizar Contato  -\n");
-    printf("    5 - Sair               -\n");
-    printf("----------------------------\n");
-    scanf("%d",&escolha);
-    switch(escolha){
-        case 1 :
-            break;
-        case 2 :
-            break;
-        case 3 :
-            break;
-        case 4 :
-            break;
-        case 5 :
-            break;
-        default:
-            printf("opção Inválida\n");
-    }
-    
+        switch(escolha)
+        {
+             case 1 :
+                    __fpurge(stdin);
+                    printf("Insira seu nome:\n");
+                    scanf("%c",nome);
+                    __fpurge(stdin);
+                    printf("Insira número de telefone:\n");
+                    scanf("%d",&num);
+                    __fpurge(stdin);
+                    printf("Insira seu endereço:\n");
+                    scanf("%c",ende);
+                    __fpurge(stdin);
+                    printf("Insira seu email:\n");
+                    scanf("%c",email);
+                    __fpurge(stdin);
+                    Item aux = insert_item(nome,num,ende,email);
+                    insert_lista(&lista,aux);
+                    break;
+            case 2 :
+                break;
+            case 3 :
+                break;
+            case 4 :
+                break;
+            case 5 :
+                break;
+            default:
+                printf("opção Inválida\n");
+        }
+}while(escolha != 5);
     //Item it1 = {90};
     //insert(&lista,it1);
     
