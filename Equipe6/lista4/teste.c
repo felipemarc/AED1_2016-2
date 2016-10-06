3c270c65ba731ca23c0dfb4a55a1f86def460172
jailsonpj <jailsoncolares@gmail.com>
2016-10-06 09:08:02 -0400

    codigo teste

--- Equipe6/lista4/teste.c
+++ Equipe6/lista4/teste.c
@@ -6,81 +6,60 @@
 int main(int argc, const char * argv[]) 
 {
    // Lista lista;
-  char nome[40];
-	int tele;
-	char ende[100];
-	char email[40];
-};
-
-typedef struct no No;
-struct no{
-	Item dados;
-	No *prox;
-	No *ant;
-};
-
-No *prim = NULL;
-No *ult = NULL;
-
-No *create()
-{
-	return NULL;
-}
-
-Item insert_item (char *name, int telefo, char *endere,char *em ) //insere as informacoes passadas pelo usuário no struct Item
-{
-    Item tempo;
-    strcpy(tempo.nome,name);
-    tempo.tele = telefo;
-    strcpy(tempo.ende,endere);
-    strcpy(tempo.email,em);
-    return tempo;
-}
-
-void lista_insert(Item it) //insere o item na lista
-{
-	No *novo;
-	novo = (No*)malloc(sizeof(No));
-	novo->dados = it;
-	novo->prox = NULL;
-	novo->ant = NULL;
-	if(prim == NULL)
-	{
-		prim = novo;
-		ult = novo;
-	}
-	else if(prim->prox == NULL)
-	{
-		ult = novo;
-		prim->prox = novo;
-		ult->ant = prim;
-	}
-	else
-	{
-		ult->prox = novo;
-		novo->ant = ult;
-		ult = novo;
-	}
-}
-
-void print_lista (No *inicio) //imprime a lista
-{
-	No *temp;
-	temp = inicio;
-	if(temp == NULL)
-	{
-		printf("Lista vazia\n");
-	}
-	
-	else{
-		while(temp != NULL)
-		{
-			printf("\tNome\t\tTel\t\tEndec\t\tEmail\n");
-			printf("\t%s",temp->dados.nome);
-			printf("\t\t%d",temp->dados.tele);
-			printf("\t\t%s",temp->dados.ende);
-			printf("\t\t%s\n",temp->dados.email);
-			temp = temp->prox;
-		}
-	}
-}
+   	char nome[40];
+   	int num;
+   	char ende[100];
+   	char email[40];
+     No *prim =create();
+     No *ult = create();
+    int escolha;
+   do
+   {
+        printf("--------Agenda AED1---------\n");
+        printf("    1 - Iserir contato     -\n");
+        printf("    2 - Mostrar Contato    -\n");
+        printf("    3 - Remover Contato    -\n");
+        printf("    4 - Atualizar Contato  -\n");
+        printf("    5 - Sair               -\n");
+        printf("----------------------------\n");
+        scanf("%d",&escolha);
+    
+        switch(escolha)
+        {
+             case 1 :
+                    __fpurge(stdin);
+                    printf("Insira seu nome:\n");
+                    scanf("%s[^\n]",&nome);
+                    __fpurge(stdin);
+                    printf("Insira número de telefone:\n");
+                    scanf("%d",&num);
+                    __fpurge(stdin);
+                    printf("Insira seu endereço:\n");
+                    scanf("%s[^\n]",ende);
+                    __fpurge(stdin);
+                    printf("Insira seu email:\n");
+                    scanf("%s[^\n]",email);
+                    __fpurge(stdin);
+                    Item aux = insert_item(nome,num,ende,email);
+                    insert_lista(aux);
+                    break;
+            case 2 :
+                print_lista(prim);
+                break;
+            case 3 :
+                break;
+            case 4 :
+                break;
+            case 5 :
+                break;
+            default:
+                printf("opção Inválida\n");
+        }
+}while(escolha != 5);
+    //Item it1 = {90};
+    //insert(&lista,it1);
+    
+   // print_lista(&lista);
+    
+  return 0;
+} 
