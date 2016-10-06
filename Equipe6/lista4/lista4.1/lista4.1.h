cf863d80199f4060dfb3ff122b2c1c546ce6c9ef
jailsonpj <jailsoncolares@gmail.com>
2016-10-05 20:25:44 -0400

    atualizando

--- Equipe6/lista4/lista4.1/lista4.1.h
+++ Equipe6/lista4/lista4.1/lista4.1.h
@@ -20,11 +20,14 @@
  * =====================================================================================
  */
 #include <stdio.h>
-#include<stdlib.h>
+#include <stdlib.h>
+#include <string.h>
+
+//typedef string Nome;
 
 typedef struct item Item;
 struct item{
-	char nome[40];
+    char nome[40];
 	int tel;
 	char endere[100];
 	char em[40];
@@ -44,15 +47,16 @@ struct lista{
 	No *atual;
 };
 
-Lista *create(Lista l)
+Lista *create(Lista *l)
 {
 	l->prim = (No*)malloc(sizeof(No));
 	l->prim->prox = NULL;
-	//l->prim->ant = NULL;
+	l->prim->ant = NULL;
 	l->ult = l->prim;
-	//l->atual = l->prim;
-	
+	l->atual = l->prim;
+
 }
+
 Item insert_item (char *name, int tele, char *ende,char *email )
 {
     Item tempo;
@@ -65,12 +69,12 @@ Item insert_item (char *name, int tele, char *ende,char *email )
 
 void insert_lista (Lista *l, Item i)
 {
+    //l->atual->prox = l->ult
 	l->ult->prox = (No*)malloc(sizeof(No));
 	while(l->atual->prox != NULL)
 	{
-        	l->atual = l->atual->prox;
-	}
-	
+       	l->atual = l->atual->prox;
+    }
 	l->atual->prox = l->ult;
 	l->ult->ant = l->atual;
 	l->ult = l->ult->prox;
@@ -78,7 +82,7 @@ void insert_lista (Lista *l, Item i)
 	l->ult->dados = i;
 }
 
-/*No * busca(Lista *l,int *k)
+No void busca(Lista *l,int k)
 {
     No *p;
     p = l->prim->prox;
@@ -87,8 +91,10 @@ void insert_lista (Lista *l, Item i)
             p = p->prox;
 
     }
+	//printf("achei\n");
     return p;
-}*/
+}
+
 int vazia (Lista *l)
 {
     if(l->prim == l->ult)
@@ -100,24 +106,30 @@ int vazia (Lista *l)
         return 0;
     }
 }
-/*void remove_lista(Lista *l,int x) 
-{
-    No *aux = busca(l,x);
-    No *tem;
-    tem = aux->ant;
-    if(vazia(l) == 1 || aux == NULL || aux == l->prim)
-    	printf("Impossivel remover\n");
-    else
-    {
-    	// Item x = aux->dados;
-	tem->prox = aux->prox;
-	if(tem->prox == NULL)
-		l->ult = tem;
-	free(aux);
+/*No* pred(Lista *l,No* r) {
+    No* p = l->prim;
+    while (p->prox != r) {
+        p = p->prox;
     }
-    
+    return p;
 }*/
 
+/*void remove_lista(Lista *l,No* r) {
+    //No *temp;
+    //temp = r->ant;
+    if(vazia(&l) == 1 || r == NULL || r == l->prim){
+        printf("impossivel remover");
+    }
+    else{
+        //x = r->dados;
+        No *temp = pred(&l,r);
+        temp->prox = r->prox;
+        if(temp->prox == NULL){
+            l->ult = temp;
+        }
+        free(r);
+    }
+}*/
 
 void print_lista (Lista *l)
 {
@@ -131,11 +143,11 @@ void print_lista (Lista *l)
 	{
 		while(temp != NULL)
 		{
-            		printf("\tNome\t\tTel\t\tEndec\t\tEmail\n");
-            		printf("\t%s ",temp->dados.nome);
-            		printf("\t\t%d ",temp->dados.tel);
-            		printf("\t\t%s",temp->dados.endere);
-            		printf("\t\t%s\n",temp->dados.em);
+            printf("\tNome\t\tTel\t\tEndec\t\tEmail\n");
+            printf("\t%s ",temp->dados.nome);
+            printf("\t\t%d ",temp->dados.tel);
+            printf("\t\t%s",temp->dados.endere);
+            printf("\t\t%s\n",temp->dados.em);
 			temp = temp->prox;
 		}
 	}
