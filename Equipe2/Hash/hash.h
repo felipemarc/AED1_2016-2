872adaccc3567cf3df995fdcc4d30278453f5285
Botinelly <lucas_botinelly@hotmail.com>
2016-10-19 13:31:31 -0400

    Entrada

--- Equipe2/Hash/hash.h
+++ Equipe2/Hash/hash.h
@@ -1,12 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include<string.h>
 
 //Função para tratar as saidas do programa
 void out(int opcao, int x, char z)
 {
-	int x;
-	char z;
-	
+		
 	switch (opcao)
 	{
 		case 1: //imprime inteiros 
@@ -18,49 +17,42 @@ void out(int opcao, int x, char z)
 		case 3: //pula uma linha
 			printf("teste nessa budega");
 			break;
+     }
 }
-
-
 /* Utilizando um ponteiro auxiliar
 * remove os espaços no vetor e
 * adiciona no vetor auxiliar */
-void remove_espacos (char *string, char *aux)
+void remove_espacos(char *string, char *aux)
 {
-  int tam;
-  tam = strlen(string);
-  int count = 0;
-  int j = 0;
-
-  for (j = 0; j <= tam; j++)
-  {
-    if (string[j] != ' ')
-    {
-      aux[count] = string[j];
-      count++;
-    }
-  }
+     int tam;
+     tam = strlen(string);
+     int count = 0;
+     int j = 0;
+     
+     for (j = 0; j <= tam; j++)
+     {
+          if (string[j] != ' ')
+          {
+               aux[count] = string[j];
+               count++;
+          }
+     }
 }
 
 //Converte de String para ASCII e soma os valores
-int string_para_ascii(const char *str)
+int string_para_ascii(char *str)
 {
-	int *aux;
-	int i=0, num;
-	aux = (int*)malloc(sizeof(int));
-    
-    while(str[i])
-    {
-    	aux[i] = str[i];
-    	i++;
-    }	
-    
-    i = 0;
-	
-	while(str[i])
-	{
-		num+=aux[i++];
-	}
-	return num;
+     int *aux;
+     int i=0, num, tam;
+     tam = strlen(str);
+     aux = (int*)malloc(sizeof(int));
+     //Joga o vetor de strings num vetor de inteiros   
+     for(i = 0; i<tam-1;i++)
+               aux[i] = str[i];
+     //Faz o somatório dos números da tabela ASCII
+     for(i = 0; i<tam-1;i++)     
+          num+=aux[i];
+     return num;
 }
 
 
---
+++ Equipe2/Hash/main.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include"hash.h"
+
+int main(int argc, const char *argv[])
+{
+     char *filme, aux[100];
+     int num;
+     filme = (char*)malloc(sizeof(char));
+     
+     printf("Qual filme vc deseja procurar ?");
+     __fpurge(stdin);
+     fgets(filme, 100, stdin);
+
+     remove_espacos(filme,aux);
+     num = string_para_ascii(filme);
+     printf("%s\n", aux);
+     printf("%d\n", num);
+     return 0;
+}
