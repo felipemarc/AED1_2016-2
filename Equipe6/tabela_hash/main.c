1c5063e9fed4a078df37a64049197fa508e17334
Jefferson A <jlda@users.noreply.github.com>
2016-10-24 11:18:44 -0400

    Update main.c

--- Equipe6/tabela_hash/main.c
+++ Equipe6/tabela_hash/main.c
@@ -3,11 +3,31 @@
 #include "tabela_atual.h"
 //#define tam 5
 
-int main (int argc,const char *argv[])
-{
-    Hash tabela;
+/* Principal */
 
-    inicia_tabela(tabela);
+int main()
+{
+    Hash *tabela;
+
+    tabela = inicia_tabela();
+    int codigo, ano;
+    char *nome;
+    int buscar;
+
+    scanf("%d %d", &codigo, &ano);
+    scanf("%s", nome);
+
+    insere_hash(tabela, codigo, nome, ano);
+    imprime_hash(tabela);
+
+    printf("\n\nBuscar: "); scanf("%d", &buscar);
+
+  /*  if (busca(tabela, buscar) != NULL)
+    {
+      printf("Achou\n\n");
+    }*/
+    Filme *achou = busca(tabela,buscar);
+    imprime_um(achou);
 
     return 0;
 }
